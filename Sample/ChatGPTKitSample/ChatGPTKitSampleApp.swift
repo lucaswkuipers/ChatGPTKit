fb6e6d9a3863f8095fc2ef1872b782880fc34e81
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2023-05-05 11:50:27 -0300

    Add sample

---
+++ Sample/ChatGPTKitSample/ChatGPTKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTKitSampleApp.swift
+//  ChatGPTKitSample
+//
+//  Created by Lucas Werner Kuipers on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
